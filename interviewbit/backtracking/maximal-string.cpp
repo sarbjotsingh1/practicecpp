8d6e0bdba403956898f57ae4d84871eac9d6baef
Gurwinder Singh <gurwinder.prvt@gmail.com>
2021-10-02 10:44:19 +0530

    Create maximal-string.cpp
    Maximal String with swaps

---
+++ interviewbit/backtracking/maximal-string.cpp
@@ -0,0 +1,35 @@
+//  Copyright © 2020 Gurwinder Singh. All rights reserved.
+// 
+//  Problem Statement:
+//  https://www.interviewbit.com/problems/maximal-string/
+//  
+//  Find multiple missing elements in a sorted array.
+//
+
+
+string maxNum;
+
+void swapAndGetMax(string A, int k) {
+    if (k <= 0) return;
+    string currentMaxNum = A;
+    for(int i = 0; i < A.length() - 1; i++) {
+        for(int j = i+1; j < A.length(); j++) {
+            string tempA = A;
+            char tempChar = tempA[i];
+            tempA[i] = tempA[j];
+            tempA[j] = tempChar;
+
+            if (stoi(maxNum) < stoi(tempA)){
+                maxNum = tempA;
+            }
+
+            swapAndGetMax(tempA, k-1);
+        }
+    }
+}
+string Solution::solve(string A, int B) {
+    maxNum = A;
+    swapAndGetMax(A, B);
+
+    return maxNum;
+}
